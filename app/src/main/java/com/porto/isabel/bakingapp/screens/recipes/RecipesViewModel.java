be2887222f27d37a551a94099cc04768e2312387
Isabel Porto <belporto@gmail.com>
2018-01-13 22:20:04 +0000

    Handling configuration changes
    Saving the list of recipes in memory inside the ViewModel.

--- app/src/main/java/com/porto/isabel/bakingapp/screens/recipes/RecipesViewModel.java
+++ app/src/main/java/com/porto/isabel/bakingapp/screens/recipes/RecipesViewModel.java
@@ -3,7 +3,11 @@ package com.porto.isabel.bakingapp.screens.recipes;
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
 
+import com.porto.isabel.bakingapp.model.baking.Recipe;
 import com.porto.isabel.bakingapp.network.BakingApi;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
@@ -14,6 +18,7 @@ public class RecipesViewModel extends ViewModel {
     private final BakingApi mBakingApi;
     private final CompositeDisposable disposables = new CompositeDisposable();
     private final MutableLiveData<RecipesResponse> response = new MutableLiveData<>();
+    private List<Recipe> mRecipes = new ArrayList<>();
 
     public RecipesViewModel(BakingApi bakingApi) {
         this.mBakingApi = bakingApi;
@@ -29,11 +34,17 @@ public class RecipesViewModel extends ViewModel {
     }
 
     public void loadRecipes() {
-        disposables.add(mBakingApi.getRecipes()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .doOnSubscribe(__ -> response.setValue(RecipesResponse.loading()))
-                .subscribe(recipes -> response.setValue(RecipesResponse.success(recipes)),
-                        throwable -> response.setValue(RecipesResponse.error(throwable))));
+        if (mRecipes.isEmpty()) {
+            disposables.add(mBakingApi.getRecipes()
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .doOnSubscribe(__ -> response.setValue(RecipesResponse.loading()))
+                    .subscribe(recipes -> {
+                        mRecipes = recipes;
+                        response.setValue(RecipesResponse.success(recipes));
+                    }, throwable -> response.setValue(RecipesResponse.error(throwable))));
+        } else {
+            response.setValue(RecipesResponse.success(mRecipes));
+        }
     }
 }
