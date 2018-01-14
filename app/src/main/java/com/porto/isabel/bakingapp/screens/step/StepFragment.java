235b3f4b0497db19690cff885860c778fa0f30c5
Isabel Porto <belporto@gmail.com>
2018-01-14 22:09:18 +0000

    Display video

--- app/src/main/java/com/porto/isabel/bakingapp/screens/step/StepFragment.java
+++ app/src/main/java/com/porto/isabel/bakingapp/screens/step/StepFragment.java
@@ -2,14 +2,26 @@ package com.porto.isabel.bakingapp.screens.step;
 
 
 import android.arch.lifecycle.ViewModelProviders;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 import com.porto.isabel.bakingapp.R;
 import com.porto.isabel.bakingapp.model.baking.Step;
 import com.porto.isabel.bakingapp.screens.details.DetailsStepViewModel;
@@ -20,10 +32,13 @@ import butterknife.ButterKnife;
 public class StepFragment extends Fragment {
 
 
+    private static final String TAG = StepFragment.class.getSimpleName();
     DetailsStepViewModel mDetailsViewModel;
 
-    @BindView(R.id.step)
-    TextView textView;
+    private SimpleExoPlayer mExoPlayer;
+
+    @BindView(R.id.playerView)
+    SimpleExoPlayerView mPlayerView;
 
     @Nullable
     @Override
@@ -41,6 +56,41 @@ public class StepFragment extends Fragment {
     }
 
     private void showStepDetails(Step step) {
-        textView.setText(step.description);
+        if (step.videoURL != null && !step.videoURL.isEmpty()) {
+            initializePlayer(Uri.parse(step.videoURL));
+        } else {
+
+        }
+    }
+
+    private void initializePlayer(Uri mediaUri) {
+        if (mExoPlayer == null) {
+            // Create an instance of the ExoPlayer.
+            TrackSelector trackSelector = new DefaultTrackSelector();
+            LoadControl loadControl = new DefaultLoadControl();
+            mExoPlayer = ExoPlayerFactory.newSimpleInstance(getContext(), trackSelector, loadControl);
+            mPlayerView.setPlayer(mExoPlayer);
+
+            // Prepare the MediaSource.
+            String userAgent = Util.getUserAgent(getContext(), "BakingApp");
+            MediaSource mediaSource = new ExtractorMediaSource(mediaUri, new DefaultDataSourceFactory(
+                    getContext(), userAgent), new DefaultExtractorsFactory(), null, null);
+            mExoPlayer.prepare(mediaSource);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        releasePlayer();
+    }
+
+    /**
+     * Release ExoPlayer.
+     */
+    private void releasePlayer() {
+        mExoPlayer.stop();
+        mExoPlayer.release();
+        mExoPlayer = null;
     }
 }
